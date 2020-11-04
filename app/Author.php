8ddaad944256251dc2f4b1f2d7a7ba8663ce4f4d
Andrea Contestabile <andreacontest@gmail.com>
2020-11-04 16:32:41 +0100

    Creazione relazioni tabelle Authors, AuthorsInfo e Comics

--- app/Author.php
+++ app/Author.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    public function info()
+    {
+
+        return $this->hasOne("App\AuthorInfo");
+    }
+
+    public function comics()
+    {
+
+        return $this->hasMany("App\Comic");
+    }
 }
--- app/AuthorInfo.php
+++ app/AuthorInfo.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuthorInfo extends Model
 {
-    //
+    public function author()
+    {
+
+        return $this->belongsTo("App\Author");
+    }
 }
